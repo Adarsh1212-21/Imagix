f74d1febee147c5258732b7c60e1defb3f968356
Adarsh1212-21 <adarshshukla1212@gmail.com>
2026-04-21 09:58:21 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const PORT=process.env.PORT||4000
 const app=express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: ['https://imagix-njfh.vercel.app', 'http://localhost:3000'],
+  credentials: true
+}));
 await connectDB()
 app.use('/api/image', imageRouter)
 
